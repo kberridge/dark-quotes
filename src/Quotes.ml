85f93d72d2ce4ff2645b930f7c73330b85bac0f2
Kevin Berridge <kevin.w.berridge@gmail.com>
2020-01-18 23:11:18 -0500

    add a quote

--- src/Quotes.ml
+++ src/Quotes.ml
@@ -9,9 +9,17 @@ type quote = {
   key: string;
 }
 
+type addQuote = {
+  quote: string;
+  attribution: string;
+}
+
 type model = {
   currentQuote: quote option;
   failed: bool;
+  addQuote: addQuote;
+  addFailed: bool;
+  addQuotePending: bool;
 }
 
 let host = "https://kberridge-test.builtwithdark.com"
@@ -24,33 +32,69 @@ let getDifferentQuoteUrl quote =
 type msg = 
   | GotQuoteResponse of (string, string Http.error) Result.t
   | GetDifferentQuote
+  | SetQuote of string
+  | SetAttribution of string
+  | PostQuote
+  | GotPostQuoteResponse of (string, string Http.error) Result.t
   [@@bs.deriving {accessors}]
+
+let apiPostQuote addQuote =
+  let open Json.Encoder in
+  let body = object_
+    [ "quote", string addQuote.quote
+    ; "attribution", string addQuote.attribution
+    ] 
+    |> encode 0
+  in
+  Http.request
+    { method' = "POST"
+    ; headers = []
+    ; url = String.concat "" [host; "/quote"]
+    ; body = Web.XMLHttpRequest.StringBody body
+    ; expect = Http.expectString
+    ; timeout = None
+    ; withCredentials = false
+    }
+  |> Http.send gotPostQuoteResponse
 
 let init() = 
   ( {
      currentQuote = None;
-     failed = false
+     failed = false;
+     addQuote = {
+       quote = "";
+       attribution = "";
+     };
+     addFailed = false;
+     addQuotePending = false;
     }
   , Http.getString getQuoteUrl |> Http.send gotQuoteResponse
   )
 
+let updateCurrentQuoteFromJson model data =
+  let open Json.Decoder in
+  let quote_decoder = map3 (fun x y z -> {key = x; quote = y; attribution = z})
+      (field "key" string)
+      (field "quote" string)
+      (field "attribution" string |> maybe) in
+  match decodeString quote_decoder data with
+  | Ok quote -> {model with currentQuote = Some quote; failed = false}
+  | Error _ -> {model with currentQuote = None; failed = true}
+
 let update (model : model) = function
-  | GotQuoteResponse (Error _e) -> {currentQuote = None; failed = true}, Cmd.none
-  | GotQuoteResponse (Ok data) -> 
-    let open Json.Decoder in
-    let quote_decoder = map3 (fun x y z -> {key = x; quote = y; attribution = z})
-        (field "key" string)
-        (field "quote" string)
-        (field "attribution" string |> maybe) in
-    begin match decodeString quote_decoder data with
-      | Ok quote -> {currentQuote = Some quote; failed = false}
-      | Error _ -> {currentQuote = None; failed = true}
-    end, Cmd.none
+  | GotQuoteResponse (Error _e) -> {model with currentQuote = None; failed = true}, Cmd.none
+  | GotQuoteResponse (Ok data) -> updateCurrentQuoteFromJson model data, Cmd.none
   | GetDifferentQuote ->
     ( model
     , Http.getString (getDifferentQuoteUrl model.currentQuote) |> Http.send gotQuoteResponse
     )
-
+  | SetQuote q -> {model with addQuote = {model.addQuote with quote = q}}, Cmd.none
+  | SetAttribution a -> {model with addQuote = {model.addQuote with attribution = a}}, Cmd.none
+  | PostQuote -> {model with addQuotePending = true}, apiPostQuote model.addQuote
+  | GotPostQuoteResponse (Error _e) -> {model with addFailed = true; addQuotePending = false}, Cmd.none
+  | GotPostQuoteResponse (Ok data) -> 
+    let model' = updateCurrentQuoteFromJson model data in
+    { model' with addQuote = { quote = ""; attribution = "" }; addQuotePending = false}, Cmd.none
 
 let viewQuote (quote: quote) =
   div
@@ -64,6 +108,28 @@ let viewQuote (quote: quote) =
         [ style "padding-left" "2em" ]
         [ text (String.concat "" ["-"; attribution]) ]
       | None -> noNode
+    ]
+
+let viewNewQuoteForm model =
+  div
+    [ style "display" "flex"
+    ; style "justify-content" "center"
+    ]
+    [
+      div []
+      [ h2 [ style "margin" "4em 0 0 0" ] [ text "Add a new quote" ]
+      ; div 
+          [ style "border" "1px solid black"
+          ; style "padding" "1em"
+          ; style "width" "40em"
+          ]
+          [ label [ for' "quote"; style "display" "block" ] [ text "Quote" ]
+          ; textarea [ name "quote"; style "width" "100%"; onChange setQuote; value model.addQuote.quote ] [ ]
+          ; label [ for' "attribution"; style "display" "block" ] [ text "Attribution" ]
+          ; input' [ name "attribution"; style "width" "100%"; onChange setAttribution; value model.addQuote.attribution ] [ ]
+          ; button [ onClick PostQuote; style "margin-top" "1em"; Attributes.disabled model.addQuotePending ] [ text "Add Quote" ]
+          ]
+      ]
     ]
 
 let view (model : model) =
@@ -88,8 +154,7 @@ let view (model : model) =
         | Some q -> 
           [ viewQuote q ]
       end
-    ; match model.currentQuote with
-      | None -> noNode
+    ; begin match model.currentQuote with
       | Some _ -> 
         div
         [ style "display" "flex"
@@ -98,6 +163,9 @@ let view (model : model) =
         ]
         [ button [ onClick GetDifferentQuote ] [ text "Get a different quote"]
         ]
+      | None -> noNode
+      end
+    ; viewNewQuoteForm model
     ]
 
 let main =
